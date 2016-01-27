70eb41dea02c0ca5e805e2e4e57ec1f10659ebd0
Nate Ricklin <nate@rickl.in>
2016-01-27 14:22:23 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='githubtools',
+      version='0.0.1',
+      author='nricklin',
+      author_email='nricklin@digitalglobe.com',
+      license='MIT',
+      description='Some github tools',
+      py_modules=['githubtools'])
