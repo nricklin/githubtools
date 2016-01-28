6bf88f4d0c8425afe7f0ac27dc70e0e3da714e12
Nate Ricklin <nate@rickl.in>
2016-01-28 11:24:22 -0700

    add ability to use personal token

--- githubtools/commit_status.py
+++ githubtools/commit_status.py
@@ -1,12 +1,14 @@
 """
 Usage:
     github-commit-status -c <commit-hash> -s <status> -u <github-username> -p <github-password> -r <github_repo> --url <URL> --context <context> -d "<description>"
+    github-commit-status -c <commit-hash> -s <status> -t <github-token> -r <github_repo> --url <URL> --context <context> -d "<description>"
 
 Options:
     -c <commit-hash>, --commit-hash <commit-hash>  Github commit hash
     -s <status>, --status <status>  Status to set [pending, success, error, or failure]
     -u <username>, --username <username>  github username
     -p <password>, --password <password>  github password
+    -t <token>, --token <token>  github access token
     -r <repository>, --repo <repository> github repository
     -d <description> --description <description> description of the status
     --url <url> URL to refer back to
@@ -24,13 +26,17 @@ def main():
 	commit = arguments['--commit-hash']
 	user = arguments['--username']
 	password = arguments['--password']
+	token = arguments['--token']
 	url = arguments['--url']
 	repo = arguments['--repo']
 	context = arguments['--context']
 	description = arguments['--description']
 	print "Setting status %s for commit %s." % (status, commit)
 
-	g = Github(user,password)
+	if token:
+		g = Github(token)
+	elif user and password:
+		g = Github(user,password)
 	r = g.get_repo(repo)
 	c = r.get_commit(commit)
 	c.create_status(status, target_url=url, description=description, context=context)
