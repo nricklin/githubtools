6d660def12d93d1444e4f3951e031c3fc40c6719
Nate Ricklin <nate@rickl.in>
2016-02-13 09:42:24 -0700

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import textwrap
 
 setup(name='githubtools',
-      version='0.3.0',
+      version='0.3.1',
       author='nricklin',
       author_email='nricklin@digitalglobe.com',
       license='MIT',
